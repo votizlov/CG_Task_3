d6f3e438cd44c76da91f3e283a6426a2d10dd90a
Votizlov <SeriousFox@yandex.ru>
2019-10-17 00:33:52 +0300

    ui updated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
+import functions.Function1;
+import functions.IFunction;
+import sample.lineDrawers.LineDrawer;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Main {
 
-    static class DrawPanel extends JPanel implements MouseMotionListener, MouseListener {
-        private Object ld;
+    static class DrawPanel extends JPanel implements MouseMotionListener, MouseListener, MouseWheelListener {
+        private LineDrawer ld;
         private ScreenConverterInterface sc;
         private Line l;
         private boolean isMouseClicked = false;
+        private IFunction currentFunction;
 
         public DrawPanel(){
             super();
@@ -19,12 +24,20 @@ public class Main {
             l = new Line(new RealPoint(0,0),new RealPoint(1,1));
         }
 
+        public void setCurrentFunction(IFunction function){
+            this.currentFunction = function;
+            repaint();
+        }
+
         @Override
         public void paint(Graphics g){
             BufferedImage bi = new BufferedImage(getWidth(),getHeight(),BufferedImage.TYPE_INT_RGB);
+
             if(ld!=null){
-                //ld.drawLine();//todo make constr for points
+                ld.drawLine(getWidth()/2,0,getWidth()/2,getHeight(),Color.CYAN);//todo make constr for points
+                ld.drawLine(0,getHeight()/2,getWidth(),getHeight()/2,Color.CYAN);
             }
+            g.drawImage(bi,0,0,null);
         }
         private ScreenPoint last = null;
         @Override
@@ -65,12 +78,19 @@ public class Main {
         public void mouseExited(MouseEvent mouseEvent) {
 
         }
+
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            e.getScrollAmount();
+        }
     }
     public static void main(String... args){
         DrawPanel myPanel = new DrawPanel();
-         JFrame drawFrame = new JFrame();
+        JFrame drawFrame = new JFrame();
         JPanel funcPanel = new JPanel();
         JFrame frame = new JFrame();
+        HashMap<JRadioButton,IFunction> buttonIFunctionHashMap = new HashMap<>();
+
 
         funcPanel.setSize(200, 500);
         frame.setSize(200, 500);
@@ -81,9 +101,19 @@ public class Main {
         funcPanel.setVisible(true);
         funcPanel.setLayout(new GridLayout(8, 2));
         boolean flag = true;
+        JRadioButton btn;
         for(int i =0;i<16;i++){
-            if(flag)
-                funcPanel.add(new JRadioButton());
+            if(flag) {
+                btn = new JRadioButton();
+                buttonIFunctionHashMap.put(btn,new Function1());
+                btn.setAction(new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        myPanel.setCurrentFunction(buttonIFunctionHashMap.get(e.getSource()));
+                    }
+                });
+                funcPanel.add(btn);
+            }
             else
                 funcPanel.add(new JTextField(i));
             flag=!flag;
